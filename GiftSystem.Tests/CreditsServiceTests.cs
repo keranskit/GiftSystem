42bfcfd8b2fe5153fa9f665955357613d6e6202e
Trayan Keranov <keranskit@gmail.com>
2020-06-25 21:21:58 +0300

    Added some testing with xUnit

---
+++ GiftSystem.Tests/CreditsServiceTests.cs
@@ -0,0 +1,35 @@
+﻿namespace GiftSystem.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Data;
+    using Data.Models;
+    using Microsoft.EntityFrameworkCore;
+    using Services;
+    using Xunit;
+
+    public class CreditsServiceTests
+    {
+        [Fact]
+        public async Task TestMultipleTask()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            var dbContext = new ApplicationDbContext(options);
+            var service = new CreditsService(dbContext);
+
+            var user1 = new ApplicationUser
+                {Id = "1", Credits = 50, PhoneNumber = "123456", Email = "pesho@abv.bg"};
+            var user2 = new ApplicationUser
+                { Id = "2", Credits = 20, PhoneNumber = "234567", Email = "gosho@abv.bg" };
+            await dbContext.Users.AddAsync(user1);
+            await dbContext.Users.AddAsync(user2);
+            await dbContext.SaveChangesAsync();
+
+            Assert.IsType<string>(await service.TransferCredits(user1, "234567", 10, "hello"));
+            Assert.Equal("Successful", await service.TransferCredits(user1, "234567", 10, "hello"));
+            Assert.Equal("Receiving user not found", await service.TransferCredits(user1, "2345678", 10, "hello"));
+            Assert.Equal("Not enough credits", await service.TransferCredits(user1, "234567", 1000, "hello"));
+        }
+    }
+}
---
+++ GiftSystem.Tests/TransfersServiceTests.cs
@@ -0,0 +1,35 @@
+﻿namespace GiftSystem.Tests
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Data.Models;
+    using Xunit;
+    using GiftSystem.Data;
+    using Services;
+
+    public class TransfersServiceTests
+    {
+        [Fact]
+        public async Task TestMultipleTask()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            var dbContext = new ApplicationDbContext(options);
+            var service = new TransfersService(dbContext);
+
+            await dbContext.Transfers.AddAsync(new Transfer { Credits = 5, ReceiverId = "pesho", SenderId = "gosho", Id = "transfer1", Message = "helloWorld" });
+            await dbContext.Transfers.AddAsync(new Transfer { Credits = 5, ReceiverId = "misho", SenderId = "mitko", Id = "transfer2", Message = "helloWorld2" });
+            await dbContext.Transfers.AddAsync(new Transfer { Credits = 5, ReceiverId = "ivan", SenderId = "dragan", Id = "transfer3", Message = "helloWorld3" });
+            await dbContext.SaveChangesAsync();
+
+            Assert.Equal(3, service.GetAll().Count());
+            Assert.IsAssignableFrom<IEnumerable<Transfer>>(service.GetAll());
+            Assert.IsType<List<Transfer>>(service.GetByReceiverId("gosho"));
+            Assert.IsType<List<Transfer>>(service.GetBySenderId("pesho"));
+            Assert.Empty(service.GetByReceiverId("hh"));
+        }
+    }
+}
